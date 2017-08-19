e5db845ea6f358c00f86449fe8851fe233bfa99e
Andrew Bagwell <andrewbagwell1@gmail.com>
2017-08-19 12:04:12 -0500

    added vertical and horizontal basic histograms

--- wordHistogram.c
+++ wordHistogram.c
@@ -11,42 +11,76 @@ or vertically
 
 #include <stdio.h>
 
-#define VERTICAL 0
-#define WORDCOUNT 5
+#define VERTICAL 1
+
 
 
 int main(int argc, char const *argv[])
 {
 
 	int i, j; 
-	int wordLengths[WORDCOUNT];
+	int wordLengths[argc-1];
 
 	for (i = 0; i < argc-1; i++) 
 		wordLengths[i] = 0; 
 
+	int max = 0; 
 
+	//get lengths of words and put into array
 	for (i = 1; i < argc; i++) {
 
 		int j;
 
-		for (j = 0; argv[i][j] != '\0'; j++)
+		for (j = 0; argv[i][j] != '\0'; j++) {
+
 			wordLengths[i-1]++;
+
+			if (wordLengths[i-1] > max)
+				max = wordLengths[i-1];
+		}
 	}
+
 	
 	//check to see which type of histogram will be printed
 
 	if (VERTICAL) {
 
+
+		for (i = (max * 2); i > 0; i--) {
+
+			for (j=0; j < argc-1; j++) {
+
+				if (wordLengths[j] == i) {
+					printf("%c\t", '*');
+					wordLengths[j]--;	
+				}
+
+			
+			}
+
+			printf("\n");
+
+		}
+
+		for (i = 1; i < argc; i++) 
+			printf("%s\t", argv[i]);
 		
+		printf("\n");
+
 
 	}
 
 	else {
 
+		for (i = 0; i < argc-1; i++) {
 
-		for (i = 0; i < argc-1; i++) 
-			printf("%d\n", wordLengths[i]);
+			printf("%s\t", argv[i+1]);
 
+			for (j = 1; j < wordLengths[i]; j++) 
+				printf("-");
+			
+			printf("\n");
+		}	
 	}
 
 	return 0;
